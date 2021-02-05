68b0069827b2534d93f6cfe4d59c5f571e67ea09
Floris Hoogenboom <floris@digitaldreamworks.nl>
2021-02-05 17:33:01 +0100

    Specify extra dependency needed by training jobs.

--- setup.py
+++ setup.py
@@ -9,5 +9,12 @@ setup(
     author='Floris Hoogenboom',
     author_email='floris@digitaldreamworks.nl',
     description='Gym environments for a variety of Airport Processes',
-    install_requires=['gym>=0.17.3']
+    install_requires=['gym>=0.17.3'],
+    extras_require={
+        'experiments': [
+            'tf-agents[reverb]==0.7.1',
+            'tensorboard==2.4.1',
+            'tensorflow==2.4.1'
+        ]
+    }
 )
